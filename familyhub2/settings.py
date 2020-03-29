2b893c916e33529b670a5825c97d9cabb9cbfe2b
Anna Greaves <gilhespy.anna@gmail.com>
2020-03-29 18:17:54 +0200

    Create env vars for secretkey and hostname.

--- familyhub2/settings.py
+++ familyhub2/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5tx@#27$()i-wil)vve4blo)sop*6wlfd3z-jubv1kfd+_ect!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
 
 
 # Application definition
