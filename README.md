c30b926d59ad7b12bc57cb16cbf00401169550dc
Anna Greaves <gilhespy.anna@gmail.com>
2020-04-11 16:36:18 +0200

    Create BusinessInfo model.

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ An English language web application for family evenings and activities for kids 
 
 Application based on a previous [student project I built](https://github.com/AJGreaves/familyhub) using Flask and MongoDB. Updated to use Django3 and SQL.
 
+## Dependencies
+- [django-crispy-forms](https://pypi.org/project/django-crispy-forms/) to style django forms with bootstrap styling
+- [django-phone-field](https://pypi.org/project/django-phone-field/) to handle validation of phone numbers entered into the database.
+
 ## Libraries
 - [Bootstrap](https://getbootstrap.com/)
 - [MDBootstrap](https://mdbootstrap.com/)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phone_field import PhoneField
 
 # Create your models here.
+class BusinessInfo(models.Model):
+    """ Model for Users Business Information """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    business_name = models.CharField(max_length=25)
+    kvk_num = models.CharField(max_length=20)
+    # logo = image
+    website = models.SlugField(max_length=50)
+    phone = PhoneField(blank=True, help_text='Contact phone number')
+    street_name = models.CharField(max_length=50)
+    street_num = models.CharField(max_length=10)
+    postcode = models.CharField(max_length=10)
+    town = models.CharField(max_length=25)
+    facebook = models.SlugField(blank=True, max_length=100)
+    instagram = models.SlugField(blank=True, max_length=100)
+    twitter = models.SlugField(blank=True, max_length=100)
+    foundation = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
