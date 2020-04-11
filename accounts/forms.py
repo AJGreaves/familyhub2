b90c7b62e7b61c18e8fdf58f1350c2d27f38954c
Anna Greaves <gilhespy.anna@gmail.com>
2020-04-11 16:48:18 +0200

    Create forms for BusinessInfo model.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import BusinessInfo
 
 class UserRegisterForm(UserCreationForm):
     """ Form to register a new user to User table """
@@ -18,3 +19,31 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'first_name', 'last_name']
+
+class BusinessRegisterForm(forms.ModelForm):
+    """ 
+    Form to collect basic business information 
+    from potential business customer
+    """
+    class Meta:
+        model = BusinessInfo
+        fields = ['business_name', 'kvk_num']
+
+class BusinessProfileForm(forms.ModelForm):
+    """ 
+    Form to add full business profile to database
+    """
+    class Meta:
+        model = BusinessInfo
+        fields = [
+            'business_name',
+            'website',
+            'phone',
+            'street_name',
+            'street_num',
+            'postcode',
+            'town',
+            'facebook',
+            'instagram',
+            'twitter'
+        ]
