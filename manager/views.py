47803e18dfc7975dcd375f28620f772cbd0347af
Anna Greaves <gilhespy.anna@gmail.com>
2020-04-14 17:17:09 +0200

    Display applications on manager page by status.

--- manager/views.py
+++ manager/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from accounts.models import Application
 
 # Create your views here.
 def manager_view(request):
-    return render(request, 'manager.html')
+    waiting_applications = Application.objects.filter(status="Awaiting response")
+    denied_applications = Application.objects.filter(status="Denied")
+    approved_applications = Application.objects.filter(status="Approved")
+    context = {
+        "waiting_applications": waiting_applications,
+        "denied_applications": denied_applications,
+        "approved_applications": approved_applications,
+    }
+    return render(request, 'manager.html', context)
+
