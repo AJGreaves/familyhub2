62d7a4cca79025b8bed320e951f90cb77e7e2671
Anna Greaves <gilhespy.anna@gmail.com>
2020-04-14 17:47:03 +0200

    Manager page form updates status of applications in database.

--- manager/views.py
+++ manager/views.py
@@ -3,6 +3,11 @@ from accounts.models import Application
 
 # Create your views here.
 def manager_view(request):
+    """
+    View to handle manager page. Loads applications and displays them with a form
+    to update their status. Processes status change from the manager to update
+    database.
+    """
     waiting_applications = Application.objects.filter(status="Awaiting response")
     denied_applications = Application.objects.filter(status="Denied")
     approved_applications = Application.objects.filter(status="Approved")
@@ -11,5 +16,12 @@ def manager_view(request):
         "denied_applications": denied_applications,
         "approved_applications": approved_applications,
     }
+
+    if request.method == "POST":
+        form = request.POST
+        Application.objects.filter(pk=form['pk']).update(status=form['status'])
+
     return render(request, 'manager.html', context)
+    
+
 
